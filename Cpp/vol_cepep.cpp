66e39832183a12970aed81cda7ffd22bcd32cadc
Juan Alegría <juanszalegria@gmail.com>
2021-02-04 07:58:18 -0500

    Feat: add daily update to cepep volume

--- Cpp/vol_cepep.cpp
+++ Cpp/vol_cepep.cpp
@@ -14,29 +14,42 @@ int solveMeFirst(int a, int b) {
     return a + b;
 }
 
-int main_first() {
+int mainFirst() {
     int num1, num2;
     int sum;
     cin >> num1 >> num2;
     sum = solveMeFirst(num1, num2);
     cout << sum;
+
     return 0;
 }
 // --------------------------------------------
-int main_hello() {
+int mainHelloWorld() {
     printf("Hello, World!");
     return 0;
 }
 // --------------------------------------------
-int main_sum() {
+int mainSum() {
     int a, b, c;
     cin >> a >> b >> c;
     int sum = a + b + c;
     cout << sum << endl;
+
     return 0;
 }
 // --------------------------------------------
+int mainDataTypes() {
+    int d;
+    long ld;
+    char ch;
+    float f;
+    double lf;
 
+    scanf("%d %ld %c %f %lf", &d, &ld, &ch, &f, &lf);
+    printf("%d\n%ld\n%c\n%f\n%lf", d, ld, ch, f, lf);
+
+    return 0;
+}
 // --------------------------------------------
 
 // --------------------------------------------
