99ec507d7cbdebd4df03b7123466b1e32531921d
Juan Alegría <juanszalegria@gmail.com>
2021-02-13 11:26:00 -0500

    Docs: add sifra context

--- Cpp/sifra.cpp
+++ Cpp/sifra.cpp
@@ -7,6 +7,20 @@
 
 using namespace std;
 
+/*
+ * Task Šifra
+    Knight Borna is trying to decipher secret letters of his enemies. He recently came into possession of a special
+    piece of paper that is used for the cipher. The paper contains a single word, made up of lowercase letters and digits.
+    The secret code is the number of distinct integers that appear in the word, when all letters are replaced with
+    whitespace. The integers are written without leading zeroes.
+    Since Borna is not very good with numbers, he turned to you for help.
+ * Input
+    The first line contains a word of length between 1 and 100. The word contains only lowercase letters and digits.
+    All integers in it have at most three digits.
+ * Output
+    Output the secret code.
+*/
+
 bool checkExists(string actual, const vector <string> &old) {
     if (actual.empty()) {
         return true;
